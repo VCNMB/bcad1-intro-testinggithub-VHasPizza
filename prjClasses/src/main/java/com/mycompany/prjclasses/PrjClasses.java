ce37dcc5deb59bccea7f39c3ce47a3aa0fbf6f1b
VHasPizza <virnerusteberg@gmail.com>
2025-03-11 10:40:16 +0200

    Added newClass

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,5 +12,13 @@ public class PrjClasses {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+         
+        
+        NewClass nc = new NewClass(); //creating an object of newClass
+       
+        nc.helloClass(); //Save to use varible name nc to communicate
+        
+        System.out.println (nc.strSecondClassVar); //output variable from second class
+        
     }
 }
